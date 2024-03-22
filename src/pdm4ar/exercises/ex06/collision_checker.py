8d636e9937381a7e214d4750023a90e8ce0f8ca6
Riccardo Orion Feingold <riccardofeingold@gmail.com>
2024-03-22 16:49:54 +0000

    collision checking algorithms

--- src/pdm4ar/exercises/ex06/collision_checker.py
+++ src/pdm4ar/exercises/ex06/collision_checker.py
@@ -10,6 +10,8 @@ from pdm4ar.exercises_def.ex06.structures import (
     Triangle,
     Path,
 )
+import shapely
+import numpy as np
 
 ##############################################################################################
 ############################# This is a helper function. #####################################
@@ -60,7 +62,38 @@ def check_collision(p_1: GeoPrimitive, p_2: GeoPrimitive) -> bool:
 
 
 ##############################################################################################
+############################# This is a helper function. #####################################
+# Feel free to use this function or not
 
+def geo_primitive_to_shapely(p: GeoPrimitive):
+    if isinstance(p, Point):
+        return shapely.Point(p.x, p.y)
+    elif isinstance(p, Segment):
+        return shapely.LineString([[p.p1.x, p.p1.y], [p.p2.x, p.p2.y]])
+    elif isinstance(p, Circle):
+        return shapely.Point(p.center.x, p.center.y).buffer(p.radius)
+    elif isinstance(p, Triangle):
+        return shapely.Polygon([[p.v1.x, p.v1.y], [p.v2.x, p.v2.y], [p.v3.x, p.v3.y]])
+    else: #Polygon
+        vertices = []
+        for vertex in p.vertices:
+            vertices += [(vertex.x, vertex.y)]
+        return shapely.Polygon(vertices)
+    
+def geo_primitive_to_shapely_with_transformation(p: GeoPrimitive, t: Point, s: float):
+    if isinstance(p, Point):
+        return shapely.Point(int((p.x - t.x)*s), int((p.y - t.y)*s))
+    elif isinstance(p, Segment):
+        return shapely.LineString([[int((p.p1.x - t.x)*s), int((p.p1.y - t.y)*s)], [int((p.p2.x - t.x)*s), int((p.p2.y - t.y)*s)]])
+    elif isinstance(p, Circle):
+        return shapely.Point(int((p.center.x - t.x)*s), int((p.center.y - t.y)*s)).buffer(p.radius)
+    elif isinstance(p, Triangle):
+        return shapely.Polygon([[int((p.v1.x - t.x)*s), int((p.v1.y - t.y)*s)], [int((p.v2.x - t.x)*s), int((p.v2.y - t.y)*s)], [int((p.v3.x - t.x)*s), int((p.v3.y - t.y)*s)]])
+    else: #Polygon
+        vertices = []
+        for vertex in p.vertices:
+            vertices += [(int((vertex.x - t.x)*s), int((vertex.y - t.y)*s))]
+        return shapely.Polygon(vertices)
 
 class CollisionChecker:
     """
@@ -86,7 +119,54 @@ class CollisionChecker:
                     obstacles (List[GeoPrimitive]): List of obstacles as GeoPrimitives
                     Please note that only Triangle, Circle and Polygon exist in this list
         """
-        return []
+        # sample points on the circle
+        max_points_on_circle = 50
+        circle_points = []
+        for i in range(max_points_on_circle):
+            angle = 2 * np.pi * i / max_points_on_circle
+            circle_points.append(Point(r * np.cos(angle), r * np.sin(angle)))
+
+        # check if one of the segments hits an obstacle
+        segments_in_collision = set()
+        for index in range(len(t.waypoints) - 1):
+            s = Segment(Point(t.waypoints[index].x, t.waypoints[index].y), Point(t.waypoints[index+1].x, t.waypoints[index+1].y))
+            
+            # define segment mathematically
+            start_point = np.array([s.p1.x, s.p1.y], dtype=np.float64)
+            end_point = np.array([s.p2.x, s.p2.y], dtype=np.float64)
+            line_direction = end_point - start_point
+            line_length = np.linalg.norm(line_direction)
+            if line_length > 0:
+                line_direction /= line_length
+
+            max_points_on_segment = int(line_length/r)
+            l = lambda s: start_point + s * line_direction
+            k = line_length/max_points_on_segment
+
+            exit_flag = False
+            for o in obstacles:
+                if check_collision(s, o):
+                    segments_in_collision.add(index)
+                    break
+                else:
+                    for i in range(max_points_on_segment + 1):
+                        center = l(k*i)
+                        
+                        for cp in circle_points:
+                            point = Point(center[0] + cp.x, center[1] + cp.y)
+                            
+                            if check_collision(point, o):
+                                segments_in_collision.add(index)
+                                exit_flag = True
+                                break
+                        
+                        if exit_flag:
+                            break
+                
+                if exit_flag:
+                    break
+        
+        return list(segments_in_collision)
 
     def path_collision_check_occupancy_grid(
         self, t: Path, r: float, obstacles: List[GeoPrimitive]
@@ -104,8 +184,70 @@ class CollisionChecker:
                     obstacles (List[GeoPrimitive]): List of obstacles as GeoPrimitives
                     Please note that only Triangle, Circle and Polygon exist in this list
         """
-        return []
+        # get min and max point to define the boundaries of the occupancy grid
+        waypoints = np.array([(p.x, p.y) for p in t.waypoints])
+        min_x, min_y = waypoints.min(axis=0)
+        max_x, max_y = waypoints.max(axis=0)
 
+        env_width = int(abs(max_x - min_x))
+        env_height = int(abs(max_y - min_y))
+        env_size = env_width if env_height < env_width else env_height
+        scale = 2.5
+        grid_size = int(env_size*scale)
+        resolution = env_size / grid_size # how many real units does a cell in the map cover in the real world
+
+        occupancy_grid = np.zeros((grid_size, grid_size), dtype=np.int8)
+        scaling = 1/resolution
+        shapely_obstacles = [geo_primitive_to_shapely(o) for o in obstacles]
+        shapely_segments = [geo_primitive_to_shapely(Segment(Point(t.waypoints[i].x, t.waypoints[i].y), Point(t.waypoints[i+1].x, t.waypoints[i+1].y))).buffer(r) for i in range(0, len(t.waypoints) - 1)]
+        
+        convert_x = lambda x: np.clip(int((x - min_x)*scaling), 0, grid_size)
+        convert_y = lambda y: np.clip(int((y - min_y)*scaling), 0, grid_size)
+        # rasterize obstacles
+        for o in shapely_obstacles:
+            o_x_min, o_y_min, o_x_max, o_y_max = o.bounds
+
+            o_x_min = convert_x(o_x_min)
+            o_y_min = convert_y(o_y_min)
+            o_x_max = convert_x(o_x_max)
+            o_y_max = convert_y(o_y_max)
+            for row in range(o_y_min, o_y_max):
+                y = min_y + row * resolution
+                for col in range(o_x_min, o_x_max):
+                    x = min_x + col * resolution
+                    real_point = shapely.Point([x, y])
+                    
+                    if o.contains(real_point):
+                        occupancy_grid[row, col] = 1
+
+        segments_in_collision = set()
+        segment_index = 0
+        for s in shapely_segments:
+            s_x_min, s_y_min, s_x_max, s_y_max = s.bounds
+
+            s_x_min = convert_x(s_x_min)
+            s_y_min = convert_y(s_y_min)
+            s_x_max = convert_x(s_x_max)
+            s_y_max = convert_y(s_y_max)
+
+            exit_flag = False
+            for row in range(s_y_min, s_y_max):
+                y = min_y + row * resolution
+                for col in range(s_x_min, s_x_max):
+                    x = min_x + col * resolution
+                    real_point = shapely.Point([x, y])
+                    if occupancy_grid[row, col] + 1 == 2:
+                        if s.contains(real_point):
+                            segments_in_collision.add(segment_index)
+                            exit_flag = True
+                            break
+                if exit_flag:
+                    break
+            
+            segment_index += 1
+        
+        return list(segments_in_collision)
+    
     def path_collision_check_r_tree(
         self, t: Path, r: float, obstacles: List[GeoPrimitive]
     ) -> List[int]:
@@ -122,7 +264,21 @@ class CollisionChecker:
                     obstacles (List[GeoPrimitive]): List of obstacles as GeoPrimitives
                     Please note that only Triangle, Circle and Polygon exist in this list
         """
-        return []
+        shapely_obstacles = [geo_primitive_to_shapely(o) for o in obstacles]
+        r_tree_obstacles = shapely.STRtree(shapely_obstacles)
+        
+        segments_in_collisions = []
+        for index in range(len(t.waypoints)):
+            if index == len(t.waypoints) - 1:
+                break
+
+            shapely_segment = geo_primitive_to_shapely(Segment(Point(t.waypoints[index].x, t.waypoints[index].y), Point(t.waypoints[index+1].x, t.waypoints[index+1].y)))
+            shapely_segment_with_buffer = shapely_segment.buffer(r)
+            indices_intersections = r_tree_obstacles.query(shapely_segment_with_buffer, predicate="intersects")
+            if len(indices_intersections) > 0:
+                segments_in_collisions += [index]
+
+        return segments_in_collisions
 
     def collision_check_robot_frame(
         self,
@@ -141,6 +297,28 @@ class CollisionChecker:
                     observed_obstacles (List[GeoPrimitive]): List of obstacles as GeoPrimitives in robot frame
                     Please note that only Triangle, Circle and Polygon exist in this list
         """
+        segment_shapely = shapely.LineString([[current_pose.p[0], current_pose.p[1]], [next_pose.p[0], next_pose.p[1]]])
+        segment_shapely_with_buffer = segment_shapely.buffer(r)
+
+        cp = current_pose.p
+        theta = current_pose.theta
+        affine_transformation = [
+            np.cos(theta),
+            -np.sin(theta),
+            np.sin(theta),
+            np.cos(theta),
+            cp[0],
+            cp[1]
+        ]
+
+        obstacle_shapely = [geo_primitive_to_shapely(o) for o in observed_obstacles]
+
+        for o in obstacle_shapely:
+            o_fixed_frame = shapely.affinity.affine_transform(o, affine_transformation)
+
+            if segment_shapely_with_buffer.intersects(o_fixed_frame):
+                return True
+
         return False
 
     def path_collision_check_safety_certificate(
@@ -161,4 +339,70 @@ class CollisionChecker:
                     obstacles (List[GeoPrimitive]): List of obstacles as GeoPrimitives
                     Please note that only Triangle, Circle and Polygon exist in this list
         """
-        return []
+        S_free = []  # Safe points without collision {name_of_point: (x, y)}
+        S_obs = []  # Points in collision with obstacles {name_of_point: (x, y)}
+        Dist = {}  # Distance map {name_of_point: d_min}
+        Certificates = {} # disctionary that for each certificate point a list of points is append which it vertifies {name_of_certificate_point: [(x1, y1), (x2, y2), ...]}
+
+        segments_in_collisions = set()
+        segments_shapely = [shapely.LineString([[t.waypoints[i].x, t.waypoints[i].y], [t.waypoints[i+1].x, t.waypoints[i+1].y]]) for i in range(len(t.waypoints) - 1)]
+        obstacles_shapely = [geo_primitive_to_shapely(o).buffer(r) for o in obstacles]
+
+        max_sample_point_per_segment = 20
+        segment_index = 0
+        for s in segments_shapely:
+            x, y = s.xy
+            
+            start_point = np.array([x[0], y[0]], dtype=np.float64)
+            end_point = np.array([x[1], y[1]], dtype=np.float64)
+            line_direction = end_point - start_point
+            line_length = np.linalg.norm(line_direction)
+            if line_length > 0:
+                line_direction /= line_length
+
+            l = lambda s: start_point + s * line_direction
+            k = line_length/max_sample_point_per_segment
+
+            for sample in range(max_sample_point_per_segment + 1):
+                p = l(k*sample)
+                point = shapely.Point([p[0], p[1]])
+
+                if S_free:
+                    distances = [shapely.distance(p, point) for p in S_free]
+                    d_min_free = np.min(distances)
+                    point_free = np.argmin(distances)
+                
+                if S_obs:
+                    distances = [shapely.distance(p, point) for p in S_obs]
+                    d_min_obs = np.min(distances)
+                    point_obs = np.argmin(distances)
+
+                if Dist != {}:
+                    if S_free != {}:
+                        if d_min_free <= Dist[S_free[point_free]]:
+                            Certificates[point] = S_free[point_free]
+                    
+                    elif S_obs != {}:
+                        if d_min_obs <= Dist[S_obs[point_obs]]:
+                            segments_in_collisions.add(segment_index)
+
+
+                distances = [point.distance(o) for o in obstacles_shapely]
+                d_obs = np.min(distances)
+                nearest_obstacle_index = np.argmin(distances)
+
+                if d_obs > 0:
+                    S_free += [point]
+                    Dist[point] = d_obs
+                    Certificates[point] = point
+                else:
+                    d_free = shapely.distance(point, obstacles_shapely[nearest_obstacle_index].boundary)
+                    S_obs += [point]
+                    Dist[point] = d_free
+                    segments_in_collisions.add(segment_index)
+
+
+            segment_index += 1
+
+            
+        return list(segments_in_collisions)
--- src/pdm4ar/exercises/ex06/collision_primitives.py
+++ src/pdm4ar/exercises/ex06/collision_primitives.py
@@ -1,5 +1,6 @@
 from pdm4ar.exercises_def.ex06.structures import *
-
+from triangle import triangulate
+import numpy as np
 
 class CollisionPrimitives:
     """
@@ -8,34 +9,187 @@ class CollisionPrimitives:
 
     @staticmethod
     def circle_point_collision(c: Circle, p: Point) -> bool:
+        point_vec = np.array([
+            [p.x],
+            [p.y]
+        ])
+        circle_vec = np.array([
+            [c.center.x],
+            [c.center.y]
+        ])
+        dist = np.linalg.norm(circle_vec - point_vec)
+
+        if dist < c.radius:
+            return True
         return False
 
     @staticmethod
     def triangle_point_collision(t: Triangle, p: Point) -> bool:
+        area_original = abs((t.v2.x - t.v1.x)*(t.v3.y - t.v1.y) - (t.v3.x - t.v1.x)*(t.v2.y - t.v1.y))
+        area1 = abs((t.v1.x - p.x)*(t.v2.y - p.y) - (t.v2.x - p.x)*(t.v1.y - p.y))
+        area2 = abs((t.v2.x - p.x)*(t.v3.y - p.y) - (t.v3.x - p.x)*(t.v2.y - p.y))
+        area3 = abs((t.v3.x - p.x)*(t.v1.y - p.y) - (t.v1.x - p.x)*(t.v3.y - p.y))
+
+        total_area = area1 + area2 + area3
+        if np.isclose(total_area, area_original):
+            return True
         return False
 
     @staticmethod
     def polygon_point_collision(poly: Polygon, p: Point) -> bool:
+        # first triangulate polygon
+        vertices = [(vertex.x, vertex.y) for vertex in poly.vertices]
+        dict_poly = {
+            "vertices": vertices
+        }
+        poly_triangulated = triangulate(dict_poly)
+        new_vertices = poly_triangulated["vertices"]
+        indices_triangle_vertices = poly_triangulated["triangles"]
+
+        for triangle in indices_triangle_vertices:
+            v1 = Point(new_vertices[triangle[0]][0], new_vertices[triangle[0]][1])
+            v2 = Point(new_vertices[triangle[1]][0], new_vertices[triangle[1]][1])
+            v3 = Point(new_vertices[triangle[2]][0], new_vertices[triangle[2]][1])
+
+            t = Triangle(v1=v1, v2=v2, v3=v3)
+
+            if CollisionPrimitives.triangle_point_collision(t, p):
+                return True
+
         return False
 
     @staticmethod
     def circle_segment_collision(c: Circle, segment: Segment) -> bool:
+        # first check if either the start or the end point is inside the circle
+        if CollisionPrimitives.circle_point_collision(c, segment.p1) or CollisionPrimitives.circle_point_collision(c, segment.p2):
+            return True
+
+        circle_center = np.array([c.center.x, c.center.y], dtype=np.float64)
+        start_point = np.array([segment.p1.x, segment.p1.y], dtype=np.float64)
+        end_point = np.array([segment.p2.x, segment.p2.y], dtype=np.float64)
+
+        start_to_end = end_point - start_point
+        start_to_circle = circle_center - start_point
+        line_length = np.linalg.norm(start_to_end)
+        if line_length > 0:
+            start_to_end /= line_length
+            dot_product = float(np.dot(start_to_end, start_to_circle))
+
+        
+        nearest_point = dot_product*start_to_end + start_point
+        d1 = np.linalg.norm(start_point - nearest_point)
+        d2 = np.linalg.norm(end_point - nearest_point)
+
+        if np.isclose(d1+d2, line_length):
+            if CollisionPrimitives.circle_point_collision(c, Point(nearest_point[0], nearest_point[1])):
+                return True
         return False
 
     @staticmethod
     def triangle_segment_collision(t: Triangle, segment: Segment) -> bool:
+        # first checking if either the start or the end point of the segment
+        # lies inside the triangle
+        if CollisionPrimitives.triangle_point_collision(t, segment.p1) or CollisionPrimitives.triangle_point_collision(t, segment.p2):
+            return True
+        
+        # define line lambda function
+        start_point = np.array([segment.p1.x, segment.p1.y], dtype=np.float64)
+        end_point = np.array([segment.p2.x, segment.p2.y], dtype=np.float64)
+        line_direction = end_point - start_point
+        line_length = np.linalg.norm(line_direction)
+        if line_length > 0:
+            line_direction /= line_length
+
+        l = lambda s: start_point + s * line_direction
+
+        max_points = 50
+        s = line_length/max_points
+        for i in range(1, max_points):
+            point = l(s*i)
+            
+            if CollisionPrimitives.triangle_point_collision(t, Point(point[0], point[1])):
+                return True
+            
         return False
 
     @staticmethod
     def polygon_segment_collision(p: Polygon, segment: Segment) -> bool:
+        # first check if start or end point is inside the polygon
+        if CollisionPrimitives.polygon_point_collision(p, segment.p1) or CollisionPrimitives.polygon_point_collision(p, segment.p2):
+            return True
+        
+        # Sample points and check with polygon_point_collision
+        start_point = np.array([segment.p1.x, segment.p1.y], dtype=np.float64)
+        end_point = np.array([segment.p2.x, segment.p2.y], dtype=np.float64)
+        line_direction = end_point - start_point
+        line_length = np.linalg.norm(line_direction)
+        if line_length > 0:
+            line_direction /= line_length
+
+        l = lambda s: start_point + s * line_direction
+
+        max_points = 50
+        s = line_length/max_points
+        for i in range(1, max_points):
+            point = l(s*i)
+            
+            if CollisionPrimitives.polygon_point_collision(p, Point(point[0], point[1])):
+                return True
+
         return False
 
     @staticmethod
     def polygon_segment_collision_aabb(p: Polygon, segment: Segment) -> bool:
+        aabb_rect = CollisionPrimitives._poly_to_aabb(p)
+        
+        # first check if start or end point is inside the polygon
+        if segment.p1.x > aabb_rect.p_min.x and segment.p1.x < aabb_rect.p_max.x and segment.p1.y > aabb_rect.p_min.y and segment.p1.y < aabb_rect.p_max.y:
+            return True
+        
+        if segment.p2.x > aabb_rect.p_min.x and segment.p2.x < aabb_rect.p_max.x and segment.p2.y > aabb_rect.p_min.y and segment.p2.y < aabb_rect.p_max.y:
+            return True
+        
+        # Sample points and check with polygon_point_collision
+        start_point = np.array([segment.p1.x, segment.p1.y], dtype=np.float64)
+        end_point = np.array([segment.p2.x, segment.p2.y], dtype=np.float64)
+        line_direction = end_point - start_point
+        line_length = np.linalg.norm(line_direction)
+        if line_length > 0:
+            line_direction /= line_length
+
+        l = lambda s: start_point + s * line_direction
+
+        max_points = 50
+        s = line_length/max_points
+        for i in range(1, max_points):
+            point = l(s*i)
+            
+            if point[0] > aabb_rect.p_min.x and point[0] < aabb_rect.p_max.x and point[1] > aabb_rect.p_min.y and point[1] < aabb_rect.p_max.y:
+                if CollisionPrimitives.polygon_point_collision(p, Point(point[0], point[1])):
+                    return True
+
         return False
 
     @staticmethod
     def _poly_to_aabb(g: Polygon) -> AABB:
         # todo feel free to implement functions that upper-bound a shape with an
         #  AABB or simpler shapes for faster collision checks
-        return AABB(p_min=Point(0, 0), p_max=Point(1, 1))
+        min_x = float('inf')
+        max_x = float('-inf')
+        min_y = float('inf')
+        max_y = float('-inf')
+
+        for vertex in g.vertices:
+            if vertex.x > max_x:
+                max_x = vertex.x
+            
+            if vertex.x < min_x:
+                min_x = vertex.x
+
+            if vertex.y > max_y:
+                max_y = vertex.y
+            
+            if vertex.y < min_y:
+                min_y = vertex.y
+        
+        return AABB(p_min=Point(min_x, min_y), p_max=Point(max_x, max_y))
